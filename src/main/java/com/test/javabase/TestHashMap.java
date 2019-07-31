cdf2d82fe29c2cfd69619a8131f5773e24b0ef4d
yizuizijiu <zhw54110@163.com>
2019-07-31 21:11:57 +0800

    update by zhw.

---
+++ src/main/java/com/test/javabase/TestHashMap.java
@@ -0,0 +1,12 @@
+package com.test.javabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMap {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("1000", "test01");
+	}
+}
