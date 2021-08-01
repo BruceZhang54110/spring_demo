c6440fd074377bf857b69fd0c02b8d14ae712c0d
BruceZhang54110 <zhw54110@163.com>
2021-08-02 00:39:48 +0800

    过滤logs (+1 squashed commits)
    Squashed commits:
    
    [20374c7] beanFactoryPostProcessor

--- src/main/java/com/at/spring/spring2020/XmlBeanFactoryTest.java
+++ src/main/java/com/at/spring/spring2020/XmlBeanFactoryTest.java
@@ -2,12 +2,16 @@ package com.at.spring.spring2020;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class XmlBeanFactoryTest {
     public static void main(String[] args) {
         BeanFactory bf = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
-        System.out.println(bf);
-        bf.getBean("message");
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //System.out.println(applicationContext);
+        System.out.println("getBean start");
+        System.out.println(bf.getBean("message"));
     }
 }
--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,9 +1,13 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        Integer a = 1000;
-        Integer b = 1000;
-        System.out.println(a==b);
+       List<String> list = new ArrayList<>();
+        list.add("111");
+        list.add("22");
+        System.out.println( 0 + (0>>1));
     }
 }
